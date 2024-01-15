3376f3123e324e0df1a556e41d5f4d35d14aca2e
CRAZy-Monk3Y <tathagatachakraborty97@gmail.com>
2024-01-15 18:40:11 +0530

    d1 v2

--- projects/chaibackend01/index.js
+++ projects/chaibackend01/index.js
@@ -1,1 +1,18 @@
-console.log("hi");
+const express = require("express");
+require('dotenv').config()
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/health", (req, res) => {
+  res.json({ message: "Server is Running..." });
+});
+
+app.listen(port, () => {
+  console.log(`App is running on http://localhost:${port}`);
+});
