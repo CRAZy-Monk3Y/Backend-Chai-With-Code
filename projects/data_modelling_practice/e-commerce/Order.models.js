8238a1c2242407b91afc0509314b4344e1c92fe6
CRAZy-Monk3Y <tathagata.chakraborty97@gmail.com>
2024-01-23 22:56:53 +0530

    data modelling

---
+++ projects/data_modelling_practice/e-commerce/Order.models.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+// mini schema
+const orderItemSchema = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
+});
+
+const orderSchema = new mongoose.Schema(
+  {
+    orderPrice: {
+      type: Number,
+      required: true,
+    },
+    customer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    orderItems: {
+      type: [orderItemSchema],
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["PROCESSING", "DISPATCHING", "OUT_FOR_DELIVERY", "DELIVERED"],
+      default: "PROCESSING",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
---
+++ projects/data_modelling_practice/todos/sub_todo.models.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const subTodoSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true } // get created & updated timestamps
+);
+
+export const SubTodo = mongoose.model("SubTodo", subTodoSchema);
