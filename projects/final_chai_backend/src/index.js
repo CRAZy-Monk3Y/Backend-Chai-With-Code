24509272fd6f75e76fae05a682e8e5ddcb46058e
CRAZy-Monk3Y <tathagata.chakraborty97@gmail.com>
2024-01-24 12:15:48 +0530

    connect to mongoDB

--- projects/final_chai_backend/src/index.js
+++ projects/final_chai_backend/src/index.js
@@ -1,1 +1,30 @@
-console.log("hello");
+import dotenv from "dotenv";
+import express from "express";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: ".env",
+});
+
+connectDB();
+
+/*
+const app = express();
+// using iffy
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.error("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`App is listening on http://localhost:${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
