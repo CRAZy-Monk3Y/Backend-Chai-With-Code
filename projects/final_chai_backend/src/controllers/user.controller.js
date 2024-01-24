673e3fc3e5ba075f02de213db6b6f0991ea3bf40
CRAZy-Monk3Y <tathagata.chakraborty97@gmail.com>
2024-01-24 19:39:24 +0530

    added: register conterller

--- projects/final_chai_backend/src/controllers/user.controller.js
+++ projects/final_chai_backend/src/controllers/user.controller.js
@@ -1,9 +1,60 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/User.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "chai aur code",
+  //   get user details from req
+  const { username, email, fullName, password } = req.body;
+  console.log({ username, email, fullName, password });
+  //   validation - not empty
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  // check if already exists: username , email
+  const existingUser = User.findOne({
+    $or: [{ email }, { username }],
   });
+  if (existingUser) {
+    throw new ApiError(409, "User with same credentials already exists");
+  }
+  //   check for images, check for avatar
+  const avatarImageLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+  if (!avatarImageLocalPath) {
+    throw new ApiError(400, "Avatar image not provided");
+  }
+  // upload them to cloudinary, avatar
+  const avatar = await uploadOnCloudinary(avatarImageLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "Avatar image not provided");
+  }
+  console.log(avatar);
+  //   create User object  - create entry in db
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+  //   remove password & refresh token field from response
+  // check for creation response
+  const savedUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!savedUser) {
+    throw new ApiError(500, "Something went wrong while registering the user");
+  }
+  // return response
+  res
+    .status(201)
+    .json(new ApiResponse(200, savedUser, "User registered successfully"));
 });
 
 export { registerUser };
--- projects/final_chai_backend/src/routes/user.routes.js
+++ projects/final_chai_backend/src/routes/user.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(registerUser);
+userRouter.route("/register").post(
+  upload.fields([
+    // using multer middlewqre for images
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  registerUser
+);
 
 export { userRouter };
