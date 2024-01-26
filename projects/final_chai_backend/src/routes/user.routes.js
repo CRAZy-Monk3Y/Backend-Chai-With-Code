0811b1c8033df99edcf2ba28c83bd5403bd4181d
CRAZy-Monk3Y <tathagata.chakraborty97@gmail.com>
2024-01-26 20:51:49 +0530

    add-route :  change-password, current-user, update-account, avatar, cover-image

--- projects/final_chai_backend/src/routes/user.routes.js
+++ projects/final_chai_backend/src/routes/user.routes.js
@@ -1,9 +1,14 @@
 import { Router } from "express";
 import {
+  changeCurrentUserPassword,
+  getCurrentUser,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -30,5 +35,17 @@ userRouter.route("/login").post(loginUser);
 // Secured Routes
 userRouter.route("/logout").post(verifyJWT, logoutUser);
 userRouter.route("/refresh-token").post(refreshAccessToken);
+userRouter.route("/change-password").post(verifyJWT, changeCurrentUserPassword);
+userRouter.route("/current-user").get(verifyJWT, getCurrentUser);
+userRouter.route("/update-account").patch(verifyJWT, updateAccountDetails);
+
+userRouter
+  .route("/avatar")
+  .patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+userRouter
+  .route("/cover-image")
+  .patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+
 
 export { userRouter };
